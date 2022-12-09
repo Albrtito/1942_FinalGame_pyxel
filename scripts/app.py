cd5462f225e96e510cff5c05ed80f86292611c71
Alberto Pascau <88436049+Albrtito@users.noreply.github.com>
2022-12-09 20:03:28 +0100

    Commit final Main

--- scripts/app.py
+++ scripts/app.py
@@ -10,7 +10,7 @@ from collisionManager import CollisionManager
 from background_manager import BackgroundManager
 from enemyManager import EnemyManager
 
-# main version, only enemies and final touches needed
+# main version: Added pyxres and waves
 class App:
     def __init__(self):
         # Classes attribute
