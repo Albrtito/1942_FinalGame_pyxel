36487bf9c39bb6fc3b2ea7ece4262a2ed57ded05
Ignaciol0 <ignacioleal0000@gmail.com>
2022-12-12 17:37:39 +0100

    Setters Finished

--- scripts/constants.py
+++ scripts/constants.py
@@ -25,11 +25,12 @@ hitbox_y = 16
 high_score_file_write = open('high_score.txt', "w")
 high_score_file_read = open("high_score.txt","r")
 """
+# Takes a list and draws all the elements
 def draw_list(list):
     for i in range(len(list)):
         list[i].draw()
 
-
+# Takes a list checks and removes all elements that have is alive false
 def update_list_and_delete(list):
     for index in range(len(list) - 1, -1, -1):
         if list[index].is_alive:
